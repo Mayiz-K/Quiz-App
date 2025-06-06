1a7e38281e914821c45d13a3d062344c00d30344
Mayiz-K <mayizuk@gmail.com>
2025-06-05 20:33:52 -0400

    fix: git issues

---
+++ script.js
@@ -0,0 +1,11 @@
+let globalIndex = 0
+let score = 0
+const questions = ["item1", "item2", "item3"];
+const answers = ["item1", "item2", "item3"];
+function quizRun() {
+
+}
+
+window.onload = function() {
+    document.getElementById("question").innerHTML = questions[0]
+}
